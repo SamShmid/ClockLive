4b1c9389e213be1cf2000f285306bd96d0d9bcf7
SamShmid <87190150+SamShmid@users.noreply.github.com>
2024-06-30 15:33:33 -0400

    README Update

--- ClockLive/components/About.js
+++ ClockLive/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Clock Live</Text>
       <Text style={styles.description}>
-        Clock Live combines a simple analog clock with all your tasks, from alarm clocks in the morning to calendar appointments, creating a complete picture of your day.
+      Clock Live combines a simple analog clock with all your tasks, from alarm clocks in the morning to calendar appointments. Our virtual daily planners get filled quickly, and it becomes hard not to look at all the tasks in a given month, week, or even day and not be paralyzed by the amount of work that needs to be done. Clock Live is an app meant to increase productivity by taking all these tasks that are across all these platforms and synthesizing that data into a simple-to-use UI which shows a concise picture of all the current tasks in appointments that need to be done in the next hour helping you stay on task and be more productive in your day.
       </Text>
     </View>
   );
